52faacf3995b7f314c7836c7399b830896fdc374
yann300 <yann.levreau@gmail.com>
2019-03-07 16:31:21 +0100

    fix settings tab

--- src/app/tabs/settings-tab.js
+++ src/app/tabs/settings-tab.js
@@ -145,11 +145,12 @@ class SettingsTab {
         ${this._view.config.themes}
       </div>`
 
+    var self = this
     function onchangeGenerateContractMetadata (event) {
-      this.config.set('settings/generate-contract-metadata', !this.config.get('settings/generate-contract-metadata'))
+      self.config.set('settings/generate-contract-metadata', !self.config.get('settings/generate-contract-metadata'))
     }
     function onchangeOption (event) {
-      this.config.set('settings/always-use-vm', !this.config.get('settings/always-use-vm'))
+      self.config.set('settings/always-use-vm', !self.config.get('settings/always-use-vm'))
     }
     function onswitch2darkTheme (event) {
       styleGuide.switchTheme('dark')
@@ -161,7 +162,7 @@ class SettingsTab {
       styleGuide.switchTheme('clean')
     }
     function onchangePersonal (event) {
-      this.config.set('settings/personal-mode', !this.config.get('settings/personal-mode'))
+      self.config.set('settings/personal-mode', !self.config.get('settings/personal-mode'))
     }
     styleGuide.switchTheme()
     return this._view.el
